d7090d0b07c818a1d678f5672940cf99763a432e
Darrion Johnson <90425357+desrucPrince@users.noreply.github.com>
2026-02-14 09:36:22 -0700

    Initial Commit

---
+++ seedTheNode/seedTheNodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  seedTheNodeApp.swift
+//  seedTheNode
+//
+//  Created by Darrion Johnson on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct seedTheNodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ seedTheNodeTests/seedTheNodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  seedTheNodeTests.swift
+//  seedTheNodeTests
+//
+//  Created by Darrion Johnson on 2/14/26.
+//
+
+import Testing
+@testable import seedTheNode
+
+struct seedTheNodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
